560ccadf842158c32695202d611e50802d52de30
suzukiayabun <suzukiayabun@gmail.com>
2025-02-26 11:42:53 +0900

    完了機能の追加

--- todo_list.rb
+++ todo_list.rb
@@ -1,24 +1,50 @@
 TODO_FILE = "todos.txt"
 
-# 既存のTo-Doを読み込む
-todos = File.exist?(TODO_FILE) ? File.readlines(TODO_FILE, chomp: true) : []
+todos = if File.exist?(TODO_FILE)
+    File.readlines(TODO_FILE, chomp: true)
+else
+    []
+end
 
 loop do
-    puts "Simple ToDo\n 1: 追加, 2: 表示, 3: 削除, 4: 終了" # 文字列
-    choice = gets.to_i # getsでユーザーの入力を受け取り、.to_iで文字列を整数に変換する。変数choiceに入力された数値を代入
+    puts "Simple ToDo\n1: 追加, 2: 表示, 3: 削除, 4: 完了にする, 5: 終了"
+    choice = gets.to_i
 
-    case choice # if文よりスッキリ書けるため、case。choiceは変数
-    when 1 # choiceが1のとき
+    case choice
+    when 1
         puts "追加するTo-Doを入力"
-        todos << gets.chomp # getsでユーザーの入力を受け取り、.chompで文字列の最後の行にある改行(\n)を削除。<<で配列に要素を追加する
+        task = gets.chomp
+        todos << "[ ] #{task}"
+
     when 2
         puts "=== To-Do リスト ==="
-        todos.each_with_index { |todo, i| puts "#{i+1}. #{todo}"} # each_with_indexで配列の要素を1つずつ取り出しながら、その要素のインデックスも取得する。
+        if todos.empty?
+            puts "（現在To-Doはありません）"
+        else
+            todos.each_with_index { |todo, i| puts "#{i+1}. #{todo}"}
+        end
+
     when 3
         puts "削除する番号を入力:"
-        index = gets.to_i - 1 # ユーザーが入力した番号(1,2,3)をインデックス(0,1,2)に変換
-        todos.delete_at(index) if index.between?(0, todos.size - 1) # 範囲内なら削除
+        index = gets.to_i - 1
+        if index.between?(0, todos.size - 1)
+            todos.delete_at(index)
+            puts "To-Doを削除しました。"
+        else
+            puts "無効な番号です。"
+        end
+
     when 4
+        puts "完了にするタスクの番号を入力:"
+        index = gets.to_i - 1
+        if index.between?(0, todos.size - 1)
+            todos[index].sub!("[ ]", "[✔️]") # 未完了 "[ ]" を 完了 "[✔️]"に変更
+            puts "To-Doを完了しました！"
+        else
+        puts "無効な入力です"
+        end
+
+    when 5
         File.open(TODO_FILE, "w") { |file| file.puts todos }
         puts "To-Doリストを保存しました。終了します。"
         break
